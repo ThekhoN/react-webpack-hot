c198910a5a42a3b57b9868d3ba6015c5c39b58ea
Ngaosathe <thekho.ngaosathe@jasperindia.local>
2017-05-02 16:48:16 +0530

    removed if hot detect in entry

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import {render} from 'react-dom';
+import './global-style.css';
 import Test from './component/test';
 
 const frameworks = ['React', 'Preact', 'Vue', 'Angular', 'Ember'];
 const mountNode = document.getElementById('root');
 
-// HMR
-if (module.hot) {
-  module.hot.accept();
-}
-
-const text = [...frameworks, 'something new'].join(', ');
+const text = [...frameworks, 'Mithril'].join(', ');
 const App = () => (<div>{text}<br /><Test /></div>);
 
 render(<App />, mountNode);
